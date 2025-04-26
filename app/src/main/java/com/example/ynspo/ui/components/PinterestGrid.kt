1e71694c8ba65811d94f062fd8627f5c22f5dcfa
MackyAlimena <128090305+MackyAlimena@users.noreply.github.com>
2025-04-26 00:18:09 -0300

    Package added

--- app/src/main/java/com/example/ynspo/ui/components/PinterestGrid.kt
+++ app/src/main/java/com/example/ynspo/ui/components/PinterestGrid.kt
@@ -1,3 +1,5 @@
+package com.example.ynspo.ui.components
+
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
