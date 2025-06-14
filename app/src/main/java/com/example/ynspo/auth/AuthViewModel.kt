1e0911ef7ae64bf5001c87e3313f496e84f13b59
MackyAlimena <128090305+MackyAlimena@users.noreply.github.com>
2025-06-14 00:59:56 -0300

    Refactor AuthViewModel to use dependency injection for BiometricAuthManager and add AuthStatusViewModel for authentication state management

--- app/src/main/java/com/example/ynspo/auth/AuthViewModel.kt
+++ app/src/main/java/com/example/ynspo/auth/AuthViewModel.kt
@@ -3,42 +3,19 @@ package com.example.ynspo.auth
 import android.content.Context
 import android.widget.Toast
 import androidx.lifecycle.ViewModel
-import com.austral.learning_android.security.BiometricAuthManager
+import com.example.ynspo.security.BiometricAuthManager
 import com.google.firebase.auth.FirebaseAuth
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import javax.inject.Inject
 
 @HiltViewModel
-class AuthViewModel : ViewModel() {
+class AuthViewModel @Inject constructor(
+    private val biometricAuthManager: BiometricAuthManager
+) : ViewModel() {
 
-    private val biometricAuthManager = BiometricAuthManager()
-    private var _isAuthenticated = MutableStateFlow(false)
-    val isAuthenticated = _isAuthenticated.asStateFlow()
-
-    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
-
-    private val _userData = MutableStateFlow(auth.currentUser)
-    val userData = _userData.asStateFlow()
-
-    fun authenticate(context: Context) {
-        biometricAuthManager.authenticate(
-            context,
-            onError = {
-                _isAuthenticated.value = false
-                Toast.makeText(context, "There was an error in the authentication", Toast.LENGTH_SHORT).show()
-            },
-            onSuccess = {
-                _isAuthenticated.value = true
-            },
-            onFail = {
-                _isAuthenticated.value = false
-                Toast.makeText(context, "The authentication failed, try again", Toast.LENGTH_SHORT).show()
-            }
-        )
-    }
-
-    private var _isAuthenticated = MutableStateFlow(false)
+    private val _isAuthenticated = MutableStateFlow(false)
     val isAuthenticated = _isAuthenticated.asStateFlow()
 
     private val auth: FirebaseAuth = FirebaseAuth.getInstance()
