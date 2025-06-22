f063db980956a000a206ed60446c986464e584b4
MackyAlimena <128090305+MackyAlimena@users.noreply.github.com>
2025-06-22 18:59:23 -0300

    Update UserProfile to make photoUrl nullable

--- app/src/main/java/com/example/ynspo/ui/profile/UserProfile.kt
+++ app/src/main/java/com/example/ynspo/ui/profile/UserProfile.kt
@@ -2,7 +2,7 @@ package com.example.ynspo.ui.profile
 
 data class UserProfile(
     val name: String,
-    val photoUrl: String,
+    val photoUrl: String?,
     val bio: String,
     val hobbies: List<String>,
 )
