738abfed6e1f27cfb1267542c50c81fde1d2eb9a
MackyAlimena <128090305+MackyAlimena@users.noreply.github.com>
2025-07-30 15:32:32 -0300

    Refactor MainActivity: remove unused notification channel creation code

--- app/src/main/java/com/example/ynspo/MainActivity.kt
+++ app/src/main/java/com/example/ynspo/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.ynspo
 import android.os.Bundle
 import android.os.Build
 import androidx.annotation.RequiresApi
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.fragment.app.FragmentActivity
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -12,18 +11,14 @@ import com.example.ynspo.ui.theme.YnspoTheme
 import com.example.ynspo.ui.theme.ThemeViewModel
 import com.google.firebase.FirebaseApp
 import dagger.hilt.android.AndroidEntryPoint
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import com.example.ynspo.notification.NOTIFICATION_CHANNEL_ID
+
 
 @AndroidEntryPoint
 class MainActivity : FragmentActivity() {
-    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         FirebaseApp.initializeApp(this)
-        createNotificationChannel()
 
         setContent {
             val themeViewModel: ThemeViewModel = hiltViewModel()
@@ -35,14 +30,5 @@ class MainActivity : FragmentActivity() {
         }
     }
 
-    private fun createNotificationChannel() {
-         val notificationChannel = NotificationChannel(
-            NOTIFICATION_CHANNEL_ID,
-            "Learning Android Notification",
-            NotificationManager.IMPORTANCE_HIGH
-        )
-        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
 
-        notificationManager.createNotificationChannel(notificationChannel)
-       }
 }
