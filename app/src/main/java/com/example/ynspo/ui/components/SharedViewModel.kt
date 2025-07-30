b11b51239ebe6a01c45b85b4f7ceaecf3219bf0f
MackyAlimena <128090305+MackyAlimena@users.noreply.github.com>
2025-07-30 16:16:05 -0300

    Refactor SharedViewModel to use InspirationItem instead of UnsplashPhoto

--- app/src/main/java/com/example/ynspo/ui/components/SharedViewModel.kt
+++ app/src/main/java/com/example/ynspo/ui/components/SharedViewModel.kt
@@ -1,9 +1,9 @@
 package com.example.ynspo.ui.components
 
 import androidx.lifecycle.ViewModel
-import com.example.ynspo.data.model.UnsplashPhoto
+import com.example.ynspo.data.model.InspirationItem
 import javax.inject.Inject
 
 class SharedViewModel @Inject constructor() : ViewModel() {
-    var selectedPhoto: UnsplashPhoto? = null
+    var selectedPhoto: InspirationItem? = null
 }
